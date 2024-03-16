ba17fa77dd832aacba0e4e144906c91f2b741a9b
lino-antonino <st183116@stud.uni-stuttgart.de>
2024-03-16 18:11:13 +0100

    Turn back

--- gettingStarted/standardTools/getTextSize.java
+++
@@ -1,7 +0,0 @@
-package gettingStarted.standardTools;
-
-public class getTextSize {
-    public static void main(String[] args){
-        System.out.println("Hey! Please enter the text when I inform you");
-    }
-}
